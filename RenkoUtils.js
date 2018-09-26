922ba27adc6a724fa984ca7a8ae9dab8b5ce759c
Jerry Kim <jerryrox@live.co.kr>
2018-09-26 18:19:29 +1200

    Added new function

--- RenkoUtils.js
+++ RenkoUtils.js
@@ -101,3 +101,15 @@ renko.lerp = function(from, to, t) {
 renko.inverseLerp = function(from, to, value) {
 	return (value - from) / (to - from);
 }
+
+/**
+ * String.format similar to C#
+ */
+if (!String.prototype.format) {
+	String.prototype.format = function() {
+		var args = arguments;
+		return this.replace(/{(\d+)}/g, function(match, number) { 
+			return typeof args[number] != 'undefined' ? args[number] : match;
+		});
+	};
+}
