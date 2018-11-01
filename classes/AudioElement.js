b02371bd1d1c0558cf65a8e2b7742e2c6654cf6e
Jerry Kim <jerryrox@live.co.kr>
2018-11-01 21:36:35 +1300

    Added comment

--- classes/AudioElement.js
+++ classes/AudioElement.js
@@ -1,40 +1,71 @@
+/**
+ * Class that uses HTML5 audio element to manage playback of audio.
+ * 
+ * Create a new instance to use this class.
+ */
 class AudioElement {
 
 	constructor(id) {
+		// Create a new audio element
 		this.audio = document.createElement("audio");
 		this.audio.setAttribute("id", id);
 
-		// Append input to parent
+		// Append element to parent
 		var parentNode = canvas.parentElement;
 		parentNode.appendChild(this.audio);
+
+		// Hide element
 		this.audio.style.visibility = "hidden";
 	}
 
+	/**
+	 * Sets the source url of the audio to play.
+	 * @param {String} source 
+	 */
 	setSource(source) {
 		this.audio.setAttribute("src", source);
 	}
 
+	/**
+	 * Sets the volume of this audio.
+	 * @param {number} volume 
+	 */
 	setVolume(volume) {
 		this.audio.volume = volume;
 	}
 
+	/**
+	 * Returns the raw DOM element being wrapped over.
+	 */
 	getElement() {
 		return this.audio;
 	}
 
+	/**
+	 * Plays audio.
+	 */
 	play() {
 		this.audio.play();
 	}
 
+	/**
+	 * Pauses audio.
+	 */
 	pause() {
 		this.audio.pause();
 	}
 
+	/**
+	 * Stops audio.
+	 */
 	stop() {
 		this.audio.currentTime = 0;
 		this.pause();
 	}
 
+	/**
+	 * Returns whether audio is playing.
+	 */
 	isPlaying() {
 		return !this.audio.paused && this.audio.currentTime > 0;
 	}
