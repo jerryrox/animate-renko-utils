757d0875d9872a091863d9d8a071b8b3247a7012
Jerry Kim <jerryrox@live.co.kr>
2018-12-25 15:41:05 +1300

    Added comments
    OOPS

--- classes/Random.js
+++ classes/Random.js
@@ -3,20 +3,34 @@
  */
 class Random {
 
+    /**
+     * Returns a value of either -1 or 1.
+     */
     sign() {
         return Math.random() < 0.5 ? -1 : 1;
     }
 
+    /**
+     * Returns a random value between min and max.
+     * @param {number} min 
+     * @param {number} max 
+     */
     range(min, max) {
         return Math.random() * (max - min) + min;
     }
 
+    /**
+     * Returns an array (length 2) of random values inside a unit circle.
+     */
     insideUnitCircle() {
         var point = this.onUnitCircle();
         var magnitude = Math.random();
         return [point[0] * magnitude, point[1] * magnitude];
     }
 
+    /**
+     * Returns an array (length 2) or random values on the edge of a unit circle.
+     */
     onUnitCircle() {
         var x = this.range(-1, 1);
         var y = this.sign() * Math.sqrt(1 - x*x);
